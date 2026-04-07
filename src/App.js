1c3db8ce1e3dc4492f51bf2bea43840f4b959761
vibesetpj <wldms123zzz@gmail.com>
2026-04-07 16:57:38 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,4 @@
-import { TDSMobileAITProvider } from '@toss/tds-mobile-ait';
-import MoneyArchiveDashboard from './MoneyArchiveDashboard';
 
-function App() {
-  return (
-    <TDSMobileAITProvider>
-      <MoneyArchiveDashboard />
-    </TDSMobileAITProvider>
-  );
-}
-
-export default App;
 import React from 'react';
 import MoneyArchiveDashboard from './MoneyArchiveDashboard';
 
