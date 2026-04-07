ae6f3c7c63c96ccecefe102b5648ee214a8fd7ad
vibesetpj <wldms123zzz@gmail.com>
2026-04-07 17:56:08 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { TDSMobileAITProvider } from '@toss/tds-mobile-ait';
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TDSMobileAITProvider>
-      <App />
-    </TDSMobileAITProvider>
+    <App />
   </React.StrictMode>
 );
